8c3ada2256c61ba255965178b36a106b09a30e5f
Reza Javadpour <reza.javadpour.eng@gmail.com>
2022-08-26 22:21:33 +0430

    Feature: Fetching articles from backend & handling exceptions & loading in Articles page

---
+++ src/core/services/articles-services.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const HTTP = axios.create({
+  baseURL: 'https://conduit.productionready.io/api/',
+  timeout: 10000,
+})
+
+export default {
+  getArticles() {
+    return HTTP.get(`articles`)
+  },
+}
--- src/views/panel/Articles.vue
+++ src/views/panel/Articles.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="articles-page">
+  <RequestHandler
+    v-if="!articles || !articles.length"
+    containerHeight="90vh"
+    @actionClick="fetchArticles()"
+    :isLoading="isLoading"
+    :hasError="hasError || articles === null"
+  ></RequestHandler>
+  <div v-else class="articles-page">
     <h1>All Posts</h1>
     <table class="table">
       <thead>
@@ -21,11 +28,13 @@
 </template>
 
 <script>
-import { articlesMockResponse } from '../../core/mock-data.js';
 import { transformArticles } from '../../core/transforms/articles.js';
+import articlesServices from '../../core/services/articles-services.js';
+import RequestHandler from '../../components/RequestHandler.vue';
 
 export default {
   name: 'Home',
+  components: {RequestHandler},
   data() {
     return {
       columns: [
@@ -34,11 +43,26 @@ export default {
         'excerpt',
         'created'
       ],
-      articles: []
+      articles: null,
+      isLoading: false,
+      hasError: false,
     }
   },
   mounted () {
-    this.articles = transformArticles(articlesMockResponse.articles)
+    this.fetchArticles();
+  },
+  methods: {
+    fetchArticles() {
+      this.isLoading = true;
+      this.hasError = false;
+      articlesServices.getArticles().then((response) => {
+        this.articles = transformArticles(response.data.articles)
+        this.isLoading = false;
+      }, () => {
+        this.hasError = true;
+        this.isLoading = false;
+      })
+    }
   }
 };
 </script>
